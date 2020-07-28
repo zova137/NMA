d1657d07f3eb81201a1e19d80bf14cafe1b07aa8
Jeffrey Erlich <jerlich@nyu.edu>
2020-07-28 14:20:40 +0800

    stuff

--- NMABehavProject4.m
+++ NMABehavProject4.m
@@ -7,7 +7,7 @@ resp=bdata(:,end);
 respt=bdata(:,4);
 reward=bdata(:,9);
 
-dir=lc-rc;dif=abs(dir);sum=lc+rc;
+dir=lc-rc;dif=abs(dir);sumC=lc+rc;
 %% 
 wst=find(reward==1&[diff(resp);1]==0);
 wsw=find(reward==1&[diff(resp);0]~=0);
@@ -25,14 +25,14 @@ sttring(find(strategy(:,2)==4))={'Loose_Switch'};
 strategy=[1,1;strategy];
 Strategy=[{'-'};sttring];
 %%
-c1t=find(sum==0);
+c1t=find(sumC==0);
 %C2: One Side Stimulus
-c2t=find(rc.*lc==0&sum~=0);
+c2t=find(rc.*lc==0&sumC~=0);
 % c2t=c2t(3:end);
 %C3: Different Contrast
-c3t=find(sum~=0&rc.*lc~=0&dif~=0);
+c3t=find(sumC~=0&rc.*lc~=0&dif~=0);
 %C4: Same Conttast
-c4t=find(dif==0&sum~=0);
+c4t=find(dif==0&sumC~=0);
 Categories=cell(length(label),1);
 Categories(c1t)={'No-Go'};
 Categories(c2t)={'One-Side'};
@@ -52,7 +52,42 @@ figure;bar(c,y)
 legend({'Win-Stay';'Win-Switch';'Loose-Stay';'Loos-Switch'});
 ylabel 'Trial counts';legend('Location','northeastoutside');
 saveas(gcf,'bar','emf')
-figure;bar(c,y./sum(y,2))
+figure;bar(c,y./sumC(y,2))
 legend({'Win-Stay';'Win-Switch';'Loose-Stay';'Loos-Switch'});
 ylabel 'Normalized Trial Counts';legend('Location','northeastoutside');
 saveas(gcf,'barnorm','emf')
+
+tt.reward = tt.reward==1;
+%% 
+gs = grpstats(tt(good,:), {'Strategy','deltaC'},'meanci','DataVars','went_left')
+gs.Strategy = categorical(gs.Strategy);
+strats = unique(gs.Strategy);
+for sx=1:4
+    ax = subplot(2,2,sx);
+    thisidx = gs.Strategy == strats(sx);
+    mci = gs.meanci_went_left(thisidx,:);
+    draw.errorplot(ax, gs.deltaC(thisidx), mci(:,2), mci(:,1));
+    title(strats(sx));
+    set(ax,'YLim',[0 1])
+    draw.xhairs(ax,'k-',0,0.5);
+end
+
+m1 = fitglm(tt(good,:),'went_left ~ deltaC','Distribution','binomial');
+m2 = fitglm(tt(good,:),'went_left ~ deltaC + Strategy','Distribution','binomial');
+m3 = fitglm(tt(good,:),'went_left ~ deltaC + Strategy + difficulty','Distribution','binomial');
+m4 = fitglm(tt(good,:),'went_left ~ deltaC + difficulty * Strategy','Distribution','binomial');
+
+m4e = fitglme(tt(good,:),'went_left ~ deltaC + difficulty * Strategy + (deltaC | mouse)','Distribution','binomial');
+
+h1 = fitglm(tt(good,:),'reward ~ difficulty','Distribution','binomial')
+h2 = fitglm(tt(good,:),'reward ~ difficulty + Strategy','Distribution','binomial')
+
+ttgood = tt(good,:);
+ttgood.h2 = predict(h2);
+
+tt.stim_stay = (tt.prev_deltaC .* tt.deltaC)>0;
+
+m5 = fitglm(tt, 'stay ~ stim_stay + prev_win * difficulty','Distribution','binomial')
+tt.m5 = predict(m5);
+
+gs = grpstats(tt,{'stim_stay','prev_win','difficulty'},'mean','DataVars',{'stay','m5'});
